9a2741ad12693fba97c8b59f8dc2c233aa978656
Vivek Murali <vivekmurali2k@gmail.com>
2020-07-15 12:57:21 +0530

    Added loading gif and reset button

--- public/js/download.js
+++ public/js/download.js
@@ -26,3 +26,13 @@ var x = setInterval(() => {
 if (window.history.replaceState) {
   window.history.replaceState(null, null, window.location.href);
 }
+
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("#loader").style.visibility = "visible";
+  } else {
+    document.querySelector("#loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,24 @@
-var form = document.querySelector('form');
-form.addEventListener('submit', function() {
-    console.log('clicked');
-	this.querySelector('input[type="submit"]')
-		.setAttribute('disabled', 'disabled');
-}, false);
+var form = document.querySelector("form");
+form.addEventListener(
+  "submit",
+  function () {
+    console.log("clicked");
+    this.querySelector('input[type="submit"]').setAttribute(
+      "disabled",
+      "disabled"
+    );
+    // document.querySelector("body").style.visibility = "hidden"
+    document.querySelector("#loader").style.display = "block";
+  },
+  false
+);
+
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("#loader").style.visibility = "visible";
+  } else {
+    document.querySelector("#loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
